68a2b9904090048cb73165093925964ad83fc938
Nowshun1124 <jiuhuoyong81@gmail.com>
2024-07-28 16:07:19 +0900

    remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_133803) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_11_060105) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_133803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
